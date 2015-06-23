f9ee944b38bb9f2f7188b7457ea3f91961318f94
Mateus Mendelson <mateus.silva@brisa.org.br>
2015-06-23 11:30:18 -0300

    Folder for Mateus added

---
+++ Mateus Mendelson/yabe/app/controllers/PostsController.java
@@ -0,0 +1,11 @@
+package controllers;
+ 
+import models.PostBO;
+import play.mvc.With;
+import controllers.CRUD.For;
+
+@Check("admin")
+@For(PostBO.class)
+@With(Secure.class)
+public class PostsController extends CRUD {    
+}
